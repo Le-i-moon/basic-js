df6467536673b98b62bc15d613b3329521f83a3b
Le-i-moon <mygodned@yahoo.com>
2022-05-02 20:42:46 +0300

    feet: Update3 basic-js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,50 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  /* throw new NotImplementedError('Not implemented');
+     remove line with error and write your code here */
+  let result = "";
+  let additionSeparator =
+    options && options.additionSeparator ? options.additionSeparator : "|";
+  let separator = options && options.separator ? options.separator : "+";
+  let addition =
+    options && options.addition !== undefined ? options.addition + "" : "";
+  let repeatTimes = options && options.repeatTimes ? options.repeatTimes : 1;
+  let additionRepeatTimes =
+    options && options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+  str = str + "";
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str;
+    if (addition !== "") {
+      for (let k = 0; k < additionRepeatTimes; k++) {
+        result += addition;
+        if (additionSeparator && k + 1 < additionRepeatTimes) {
+          result += additionSeparator;
+        }
+      }
+    }
+    if (separator && i + 1 < repeatTimes) {
+      result += separator;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  /* throw new NotImplementedError('Not implemented');
+     remove line with error and write your code here */
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [];
+    for (let k = 0; k < matrix[i].length; k++) {
+      let check = 0;
+      if (i > 0) {
+        if (k > 0) {
+          if (matrix[i - 1][k - 1]) check++;
+        }
+        if (matrix[i - 1][k]) check++;
+        if (k < matrix[i - 1].length - 1) {
+          if (matrix[i - 1][k + 1]) check++;
+        }
+      }
+
+      if (k > 0) {
+        if (matrix[i][k - 1]) check++;
+      }
+      if (k < matrix[i].length - 1) {
+        if (matrix[i][k + 1]) check++;
+      }
+
+      if (i < matrix.length - 1) {
+        if (k > 0) {
+          if (matrix[i + 1][k - 1]) check++;
+        }
+        if (matrix[i + 1][k]) check++;
+        if (k < matrix[i + 1].length - 1) {
+          if (matrix[i + 1][k + 1]) check++;
+        }
+      }
+      result[i][k] = check;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
